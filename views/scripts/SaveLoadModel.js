2ecd7934e6406a29b36ebb583d4e4c0fd276f0ca
CapsE <larskrafft@yahoo.de>
2014-10-02 10:14:20 +0200

    Farbpalette eingebaut

--- views/scripts/SaveLoadModel.js
+++ views/scripts/SaveLoadModel.js
@@ -45,6 +45,7 @@
 		function safe(){
 			var out = autoSafe();
 			document.getElementById("safeCode").value = out;
+			document.getElementById("safeCode").style.display = "block";
 			getImageData = true;
 			render();
 			
--- views/scripts/graphics.js
+++ views/scripts/graphics.js
@@ -123,3 +123,13 @@ function buildUGVC(s, xa, ya, za){
 	}
 	return new THREE.Mesh(model, new THREE.MeshFaceMaterial(materials));
 }
+
+function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
