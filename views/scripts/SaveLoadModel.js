0dce4497a163f67af5a8f09a6c0894db4e088e30
CapsE <larskrafft@yahoo.de>
2014-10-22 17:01:50 +0200

    Farbwerte werden jetzt korrekt gespeichert

--- views/scripts/SaveLoadModel.js
+++ views/scripts/SaveLoadModel.js
@@ -65,7 +65,11 @@
 			var keys = Object.keys(blocks);
 			for(var i = 0; i < keys.length; i++){
 				if(colors[keys[i]] != null){
-					out += keys[i].toString() + ":" + colors[keys[i]].toString(16) + ";\n";
+					var col = colors[keys[i]].toString(16);
+					while(col.length < 6){
+						col = "0" + col;
+					}
+					out += keys[i].toString() + ":" + col + ";\n";
 				}
 			}
 			return out;
